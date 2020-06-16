4c8af0283f6f111ae6bc7b07dccc10ca495b7d75
Idrimi <gerber@byrds.ch>
2020-06-16 11:44:45 +0200

    Update Plugin.m

--- ios/Plugin/Plugin.m
+++ ios/Plugin/Plugin.m
@@ -5,4 +5,6 @@
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(ContactsPlugin, "ContactsPlugin",
            CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(getPermissions, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(getContacts, CAPPluginReturnPromise);
 )
