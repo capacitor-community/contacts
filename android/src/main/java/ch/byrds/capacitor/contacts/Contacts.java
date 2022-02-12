6b1baeb884af2c64b818521aafff720639b2640a
Frak Lopez <fraklopez@gmail.com>
2022-02-11 17:25:45 -0800

    remove offending line

--- android/src/main/java/ch/byrds/capacitor/contacts/Contacts.java
+++ android/src/main/java/ch/byrds/capacitor/contacts/Contacts.java
@@ -30,8 +30,6 @@ import org.json.JSONObject;
 
 @CapacitorPlugin(
     name = "Contacts",
-    //requestCodes is labeled as legacy in bridge
-    requestCodes = Contacts.REQUEST_CODE,
     permissions = { @Permission(strings = { Manifest.permission.READ_CONTACTS, Manifest.permission.WRITE_CONTACTS }, alias = "contacts") }
 )
 public class Contacts extends Plugin {
