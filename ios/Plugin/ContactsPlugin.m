42c716d21d7c83e140b26eacb668b75c8be8f6bb
Tafel <35837839+tafelnl@users.noreply.github.com>
2023-01-18 09:52:02 +0100

    fix(ios): add missing macros for permission methods (#93)

--- ios/Plugin/ContactsPlugin.m
+++ ios/Plugin/ContactsPlugin.m
@@ -4,6 +4,8 @@
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(ContactsPlugin, "Contacts",
+           CAP_PLUGIN_METHOD(checkPermissions, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(requestPermissions, CAPPluginReturnPromise);
            CAP_PLUGIN_METHOD(getContact, CAPPluginReturnPromise);
            CAP_PLUGIN_METHOD(getContacts, CAPPluginReturnPromise);
            CAP_PLUGIN_METHOD(createContact, CAPPluginReturnPromise);
