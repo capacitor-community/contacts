e77cbc05fef1593e48d0f0e23ed0434b222df789
Tafel <35837839+tafelnl@users.noreply.github.com>
2024-04-17 13:54:26 +0200

    feat: add SPM support

--- Package.swift
+++ Package.swift
@@ -6,18 +6,18 @@ let package = Package(
     platforms: [.iOS(.v13)],
     products: [
         .library(
-            name: "ContactsPlugin",
+            name: "CapacitorCommunityContacts",
             targets: ["ContactsPlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor6-spm-test.git", branch: "main")
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", branch: "main")
     ],
     targets: [
         .target(
             name: "ContactsPlugin",
             dependencies: [
-                .product(name: "Capacitor", package: "capacitor6-spm-test"),
-                .product(name: "Cordova", package: "capacitor6-spm-test")
+                .product(name: "Capacitor", package: "capacitor-swift-pm"),
+                .product(name: "Cordova", package: "capacitor-swift-pm")
             ],
             path: "ios/Sources/ContactsPlugin"),
         .testTarget(
