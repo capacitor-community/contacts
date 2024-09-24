06dd3345265f50762b02634fa16ad445783f693c
Robin Genz <mail@robingenz.dev>
2024-09-24 12:38:18 +0200

    add missing `TAG`

--- android/src/main/java/getcapacitor/community/contacts/ContactsPlugin.java
+++ android/src/main/java/getcapacitor/community/contacts/ContactsPlugin.java
@@ -28,6 +28,8 @@ import java.util.concurrent.Executors;
 )
 public class ContactsPlugin extends Plugin {
 
+    public static final String TAG = "Contacts";
+
     private Contacts implementation;
 
     @Override
