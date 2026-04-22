8c5487b3aeda244542a50e134e2bc6ee1fa13bf6
Tafel <35837839+tafelnl@users.noreply.github.com>
2026-04-22 11:24:49 +0200

    feat(ios): add SPM support (#157)
    Closes https://github.com/capacitor-community/contacts/issues/142
    Closes https://github.com/capacitor-community/contacts/pull/145

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "CapacitorCommunityContacts",
+    platforms: [.iOS(.v14)],
+    products: [
+        .library(
+            name: "CapacitorCommunityContacts",
+            targets: ["ContactsPlugin"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0")
+    ],
+    targets: [
+        .target(
+            name: "ContactsPlugin",
+            dependencies: [
+                .product(name: "Capacitor", package: "capacitor-swift-pm"),
+                .product(name: "Cordova", package: "capacitor-swift-pm")
+            ],
+            path: "ios/Sources/ContactsPlugin"),
+        .testTarget(
+            name: "ContactsPluginTests",
+            dependencies: ["ContactsPlugin"],
+            path: "ios/Tests/ContactsPluginTests")
+    ]
+)
